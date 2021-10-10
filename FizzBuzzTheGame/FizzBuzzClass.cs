889f099612365b65a4c6d95980ad79f191a34a49
marcingluszek <gluszek.marcin@gmail.com>
2021-10-11 00:21:04 +0200

    Add project files.

---
+++ FizzBuzzTheGame/FizzBuzzClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzTheGame
+{
+    class FizzBuzzClass
+    {
+        public int UserNum;
+
+        public string FizzBuzz()
+        {
+            /*
+                -Jeżeli liczba użytkownika będzie podzielna przez 3 bez reszty, to powinien zostać zwrócony wynik „Fizz”.
+                -Jeżeli liczba użytkownika będzie podzielna przez 5 bez reszty, to powinien zostać zwrócony wynik „Buzz”.
+                -Jeżeli liczba użytkownika będzie podzielna przez 3 i przez 5 jednocześnie bez reszty, to powinien zostać zwrócony wynik „FizzBuzz”.
+                -Jeżeli liczba użytkownika nie będzie podzielna przez 3 ani przez 5 bez reszty, to wtedy zwracasz podaną liczbę.
+            */
+
+            if(UserNum % 3 == 0 && UserNum % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (UserNum % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (UserNum % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return UserNum.ToString();
+            }
+
+        }
+        
+        
+        
+
+
+    }
+}
---
+++ FizzBuzzTheGame/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+Napisz aplikację, grę FizzBuzz. Gra ta na podstawie przekazanej liczby zwraca odpowiedni wynik.
+Zasady gry:
+-Jeżeli liczba użytkownika będzie podzielna przez 3 bez reszty, to powinien zostać zwrócony wynik „Fizz”.
+-Jeżeli liczba użytkownika będzie podzielna przez 5 bez reszty, to powinien zostać zwrócony wynik „Buzz”.
+-Jeżeli liczba użytkownika będzie podzielna przez 3 i przez 5 jednocześnie bez reszty, to powinien zostać zwrócony wynik „FizzBuzz”.
+-Jeżeli liczba użytkownika nie będzie podzielna przez 3 ani przez 5 bez reszty, to wtedy zwracasz podaną liczbę.
+Niech to będzie nowa metoda niestatyczna w osobnej klasie o nazwie FizzBuzz. Najlepiej jak sama metoda będzie zwracała string’a, który zostanie zwrócony i wyświetlony w klasie Program w metodzie Main.
+*/
+
+namespace FizzBuzzTheGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var FizzBuzzObj = new FizzBuzzClass();
+
+            Console.WriteLine("Welcome in the FizzBuzzTheGame!\nPress ALT+F4 if you want to close the app.");
+
+            while (true)
+            {
+                Console.WriteLine("\nType your number:");
+                if (int.TryParse(Console.ReadLine(), out int userNum))
+                {
+                    Console.WriteLine("It's the number!");
+
+                    FizzBuzzObj.UserNum = userNum;
+
+                    Console.WriteLine(FizzBuzzObj.FizzBuzz());
+
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("It's not the number (or this number out of range)!");
+                    continue;
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
